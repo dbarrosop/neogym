32490b746262204a36c012aaa9fc9a4056df9905
David Barroso <6246622+dbarrosop@users.noreply.github.com>
2026-05-26 08:40:04 +0200

    use constellation (#22)

---
+++ backend/gen.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -euo pipefail
+
+DST_PATH="./schemas"
+
+mkdir -p "$DST_PATH"
+
+SUBDOMAIN="local"
+REGION="local"
+ADMIN_SECRET="nhost-admin-secret"
+
+ROLES=(admin user public)
+
+HASURA_URL="https://${SUBDOMAIN}.hasura.${REGION}.nhost.run/v1/graphql"
+CONSTELLATION_URL="https://${SUBDOMAIN}.graphql.${REGION}.nhost.run/v1/graphql"
+
+for role in "${ROLES[@]}"; do
+    nhost schema dump \
+        --role "${role}" --admin-secret "${ADMIN_SECRET}" \
+        -u "${HASURA_URL}" -o "${DST_PATH}/schema.hasura.${role}.graphqls"
+
+    nhost schema dump \
+        --role "${role}" --admin-secret "${ADMIN_SECRET}" \
+        -u "${CONSTELLATION_URL}" -o "${DST_PATH}/schema.nhost.${role}.graphqls"
+
+    nhost schema diff \
+        -a "${DST_PATH}/schema.hasura.${role}.graphqls" \
+        -b "${DST_PATH}/schema.nhost.${role}.graphqls" > "${DST_PATH}/schema.${role}.diff"
+done
